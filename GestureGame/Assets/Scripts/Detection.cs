2828c7d847c0dad7a7883dec1d8626f3c1d46475
JaviSaba7 <javiersabariego@hotmail.com>
2018-03-14 15:42:09 +0100

    GestureGame
    - Working on basics.

---
+++ GestureGame/Assets/Scripts/Detection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Detection : MonoBehaviour {
+
+    public float level;
+    public GameObject scene;
+    public bool stop;
+    public bool reanude;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(stop)
+        {
+            scene.GetComponent<Animator>().enabled = false;
+            Debug.Log("Stop");
+        }
+
+       /* if (reanude)
+        {
+            scene.GetComponent<Animator>().enabled = true;
+            Debug.Log("Reanude");
+        }*/
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            stop = true;
+            if (Input.GetKey(KeyCode.P))
+            {
+                reanude = true;
+                stop = false;
+            }
+        }
+    }
+
+   
+
+
+
+}
